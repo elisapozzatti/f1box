26f2450b9b3213ce5bea4a50345797e694c405cf
elisapozzatti <elisa.pozzatti@gmail.com>
2026-04-17 12:59:50 +0200

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,6 +29,8 @@ app.use(driversRoute.routes()).use(driversRoute.allowedMethods());
 app.use(teamsRoute.routes()).use(teamsRoute.allowedMethods());
 app.use(circuitsRoute.routes()).use(circuitsRoute.allowedMethods());
 
-app.listen(3000, () => {
-  console.log("Koa server running on http://localhost:3000");
+const PORT = process.env.PORT || config.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
