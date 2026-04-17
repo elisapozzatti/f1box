bd8ffbb7e6a1b513e5312f30574f9eba4a5db0a3
elisapozzatti <elisa.pozzatti@gmail.com>
2026-04-17 14:50:51 +0200

    .

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,9 +1,9 @@
 import { config } from "../config/config.js";
 import mongoose from "mongoose";
 
-export const dbClient = () => {
+export const dbClient = async () => {
   try {
-    mongoose.connect(config.DB_URL);
+    await mongoose.connect(config.DB_URL);
     console.log("Database connected");
   } catch (err) {
     console.error("Database connection error:", err);
